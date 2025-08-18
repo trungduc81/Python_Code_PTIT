b1b3194c943db672850c410664d8017f45b084b0
Trần Đức Trung <ductrung081105@gmail.com>
2025-08-19 00:27:18 +0700

    Create PY01036 - TÍNH TỔNG PHÂN THỨC.py

---
+++ PY01036 - TÍNH TỔNG PHÂN THỨC.py
@@ -0,0 +1,14 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    sum = 0
+    if n%2==0:
+        for i in range(2,n+1,2):
+            sum += 1/i
+    else :
+        for i in range(1,n+1,2):
+            sum += 1/i
+    print("%.6f" % sum)
+
+
+
